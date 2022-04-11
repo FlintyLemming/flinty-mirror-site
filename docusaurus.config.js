126d1e2676147742c16b5560d681f77fdde95739
FlintyLemming <admin@flinty.moe>
2022-04-11 11:09:06 +0800

    change folder location

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,15 +6,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'Flinty Mirror Site',
+  tagline: 'Pure Experience',
+  url: 'https://mirror.flinty.moe',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'FlintyLemming', // Usually your GitHub org/user name.
+  projectName: 'flinty-mirror-site', // Usually your repo name.
 
   presets: [
     [
